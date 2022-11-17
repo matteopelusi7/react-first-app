cd6c9bc47c9da2d385d3c1ee6d7b0ca02e58fe73
matteopelusi7 <pelusimatteo02@gmail.com>
2022-11-17 12:32:01 +0100

    add event

--- src/Product.js
+++ src/Product.js
@@ -3,6 +3,11 @@ import React from 'react'
 const img = "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f13085.jpg";
 
 const Product = ({img, prezzo, name, prezzoFinale}) => {
+
+  const handle = () => {
+    alert('hai acquistato questo prodotto!')
+  };
+
   return (
     <div className='card'>
         <img src={img} />
@@ -14,7 +19,7 @@ const Product = ({img, prezzo, name, prezzoFinale}) => {
         <div className='bet'>
             <p>PUNTA</p>
         </div>
-        <button>{prezzoFinale}</button>
+        <button onClick={handle}>{prezzoFinale}</button>
     </div>
   )
 }
