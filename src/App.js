9ec1a3baca1c1cd145723c62f326f60d50367af6
matteopelusi7 <pelusimatteo02@gmail.com>
2022-11-17 12:16:02 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 // import Componente1 from "./Componente1";
 import Product from "./Product";
-
-const primaCard = {
-  img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f13085.jpg',
-  prezzo: '2.16',
-  name: 'bukinn',
-  prezzoFinale: 14.00
-}
-
-const secondaCard = {
-  img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f13085.jpg',
-  prezzo: '2.74',
-  name: 'fran54',
-  prezzoFinale: 16.00
-}
+import products from "./products";
 
 function App() {
   return (
@@ -21,8 +8,9 @@ function App() {
       <h1>Hello React!</h1>
       {/* <Componente1 /> */}
       <div className="container-product">
-        <Product {...primaCard}/>
-        <Product {...secondaCard}/>
+        {products.map((prodotto) => {  
+          return <Product key={prodotto.id} {...prodotto} />     
+        })}
       </div>
     </div>
   );
---
+++ src/products.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+      id: 1,
+      img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f13085.jpg',
+      prezzo: '2.16',
+      name: 'bukinn',
+      prezzoFinale: 14.00
+    },
+    {
+      id: 2,
+      img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f13085.jpg',
+      prezzo: '2.74',
+      name: 'fran54',
+      prezzoFinale: 16.00
+    }
+];
+
+export default products
