97d77c3c73d50c33458502eedf4b7a99a88560a2
matteopelusi7 <pelusimatteo02@gmail.com>
2022-11-17 11:28:12 +0100

    add props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 // import Componente1 from "./Componente1";
 import Product from "./Product";
 
+const primaCard = {
+  img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f13085.jpg',
+  prezzo: '2.16',
+  name: 'bukinn',
+  prezzoFinale: 14.00
+}
+
+const secondaCard = {
+  img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f13085.jpg',
+  prezzo: '2.74',
+  name: 'fran54',
+  prezzoFinale: 16.00
+}
+
 function App() {
   return (
     <div className="App">
       <h1>Hello React!</h1>
       {/* <Componente1 /> */}
       <div className="container-product">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <Product 
+          img={primaCard.img} 
+          prezzo={primaCard.prezzo} 
+          name={primaCard.name} 
+          prezzoFinale={primaCard.prezzoFinale}/>
+        <Product 
+          img={secondaCard.img} 
+          prezzo={secondaCard.prezzo} 
+          name={secondaCard.name} 
+          prezzoFinale={secondaCard.prezzoFinale}/>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const img = "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f13085.jpg";
 
-const Product = () => {
+const Product = (props) => {
   return (
     <div className='card'>
-        <img src={img} />
+        <img src={props.img} />
         <p>200$ Bidoo Shop</p>
         <hr/>
-        <p className='green'>0,60$</p>
-        <p>kicciei</p>
+        <p className='green'>{props.prezzo}</p>
+        <p>{props.name}</p>
         <p className='red'>00:33</p>
         <div className='bet'>
             <p>PUNTA</p>
         </div>
-        <button>200$</button>
+        <button>{props.prezzoFinale}</button>
     </div>
   )
 }
